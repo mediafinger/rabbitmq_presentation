f507689f3d545af066a1cb90da754db05e048aae
Andreas Finger <andy@mediafinger.com>
2019-04-06 02:34:29 +0200

    Rescue publishing errors

--- demo_app_with_hutch/app.rb
+++ demo_app_with_hutch/app.rb
@@ -34,6 +34,7 @@ class App < Sinatra::Application
     routing_keys = params.fetch("routing_keys", "barcelona.ruby.event")
     message      = { message: params["message"] }
 
+    # Publish message:
     result = Client.publish(routing_keys: routing_keys, message: message)
 
     # For demonstration only:
@@ -41,12 +42,13 @@ class App < Sinatra::Application
     queue          = result.channel.queues.find { |name, _q| name == queue_name }&.last
     messages_count = queue&.message_count || 0
 
-    if true # check for success
-      haml :publish, locals: { success: "Queue #{queue_name} has now #{messages_count} messages." }
-    else
-      haml :publish, locals: {
-        failure: "Error: the following message has not been delivered: #{params[:message]} to #{routing_keys}.",
-      }
-    end
+    # TODO: check for success
+    haml :publish, locals: { success: "Queue #{queue_name} has now #{messages_count} messages." }
+
+  # TODO: rescue other possible errors
+  rescue ArgumentError => e
+    haml :publish, locals: {
+      failure: "Error: the following message has not been delivered: '#{params[:message]}' to #{routing_keys}. Error: #{e}",
+    }
   end
 end
